5d53f71c23fbc929505e73a2b611652536032965
Colton Ehrman <coltonje95@gmail.com>
2024-03-21 15:16:11 -0500

    feat: setup basic chat UI

--- client/App.tsx
+++ client/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="chat-screen">
+        <ul>
+          <li className="my-message">Message 1</li>
+          <li className="stranger-message">Message 2</li>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+      <form method="POST" action="/chat">
+        <input type="text" name="message" />
+        <button type="submit">Send</button>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
