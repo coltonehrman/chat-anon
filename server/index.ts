148baf5c78d731577d9a391e0985028a7050415d
Galacticai <24789792+Galacticai@users.noreply.github.com>
2024-03-22 07:30:09 +0200

    feat: Pairing users with socket.io

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,9 @@ import http from "http";
 import express from "express";
 import ViteExpress from "vite-express";
 import { Server } from "socket.io";
+import Events from "./socket.io/Events";
+import UserConnectionManager from "./controllers/UserConnector";
+import SocketUser from "./models/SocketUser";
 
 const app = express();
 
@@ -9,6 +12,29 @@ const httpServer = http.createServer(app);
 const io = new Server(httpServer);
 const port: number = //? avoids extra string->number parsing
   typeof process.env.PORT == "undefined" ? 3000 : parseInt(process.env.PORT);
+
+const userConnectionManager = new UserConnectionManager();
+
+io.on(Events.join, (socket) => {
+  const user = SocketUser.createRandomWithSocket(socket.id);
+  userConnectionManager.addUser(user);
+
+  const userConnectedTo = userConnectionManager.connectToWaitingUser(user);
+  if (userConnectedTo) {
+    //? Notify both users that they are paired now
+    io.to(user.socketId).emit(Events.paired, userConnectedTo.id);
+    io.to(userConnectedTo.socketId).emit(Events.paired, user.id);
+  } else {
+    //? Broadcast that a this user is waiting
+    io.emit(Events.waiting, user.id);
+  }
+
+  io.on(Events.leave, () => {
+    if (userConnectedTo) userConnectionManager.disconnectUsers(user, user);
+    userConnectionManager.removeUser(user);
+  });
+});
+
 httpServer.listen(port, () => {
   console.log("Server is listening...");
 });
