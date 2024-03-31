9393a54ec012546035969b54a37bfd08ba359487
Tai Luong <tailuongc@gmail.com>
2024-03-30 20:43:01 -0700

    Simple NSFW filter created

--- client/chat/ChatInterface.tsx
+++ client/chat/ChatInterface.tsx
@@ -2,10 +2,28 @@ import { useEffect, useState } from "react";
 
 import { Header } from "../components/Header";
 import { socket } from "../socket";
+import { text } from "stream/consumers";
 
 function ChatInterface() {
   const [textMessage, setTextMessage] = useState("");
   const [messages, setMessages] = useState<any[]>([]);
+
+  const [profanityList, setProfanityList] = useState<string[]>([]);
+
+  useEffect(() => {
+    fetch("NSFWList.txt")
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to load the list!')
+        }
+        return response.text();
+      })
+      .then((text) => {
+        const list = (text).split("\n").map((word: string) => word.trim());
+        setProfanityList(list);
+      })
+      .catch(error => console.error("Error loading profanity list: ", error));
+  }, []);
 
   useEffect(() => {
     socket.emit("joinRoom");
@@ -29,9 +47,28 @@ function ChatInterface() {
 
   const sendMessage: React.FormEventHandler<HTMLFormElement> = (event) => {
     event.preventDefault();
-    socket.emit("sendMessage", textMessage);
+    const filteredMessage = filterProfanity(textMessage);
+    // socket.emit("sendMessage", textMessage);
+    socket.emit("sendMessage", filteredMessage);
     setTextMessage("");
   };
+
+  const filterProfanity = (message: string): string => {
+    let listMessage = message.split(" ");
+    let filteredMessage = "";
+    for (let j = 0; j < profanityList.length; j++) {
+      for (let i = 0; i < listMessage.length; i++) {
+        if (profanityList[j] === listMessage[i]) {
+          listMessage[i] = "***";
+        }
+      }
+    }
+    for (let i = 0; i < listMessage.length; i++) {
+      filteredMessage += (listMessage[i] + " ");
+    }
+    console.log(filteredMessage);
+    return filteredMessage;
+  }
 
   return (
     <>
